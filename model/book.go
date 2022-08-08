1022b8ee58b01d110da6c39b00945ecdc4c2de8b
Barbiturate <romaenot1997@gmail.com>
2022-08-09 01:05:19 +0300

    book model db settings

--- model/book.go
+++ model/book.go
@@ -1,1 +1,15 @@
 package model
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type Book struct {
+	ID          primitive.ObjectID `bson:"_id"`
+	Author      string             `json:"author" validate:"required"`
+	Title       string             `json:"title" validate:"required"`
+	Description string             `json:"description" validate:"required"`
+	Created     time.Time          `json:"created_at"`
+	Updated     time.Time          `json:"updated_at"`
+}
