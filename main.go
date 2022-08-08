6e8e329d039a1ea8aab6bbc483ae7b03369a2489
Barbiturate <romaenot1997@gmail.com>
2022-08-09 01:32:04 +0300

    import routes to main

--- main.go
+++ main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"book-app/routes"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	router := gin.Default()
+
+	routes.BookRoutes(router)
+
+	router.Run(":" + port)
+
+}
